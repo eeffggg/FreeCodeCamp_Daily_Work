946864f23d21768454927e04383d643f7282bab5
eeffggg <79692191+eeffggg@users.noreply.github.com>
2026-03-31 15:19:43 +0800

    Create main2.js

---
+++ 3_31_JS_Robot_Self-Introduction/main2.js
@@ -0,0 +1,14 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Nana";
+let botLocation = "China";
+let favoriteLanguage = "Chinese";
+console.log("My name is "+botName+" and I live on "+botLocation+".");
+console.log("My favorite programming language is "+favoriteLanguage+".");
+let codingFact = "Programming did not originate from the efforts of the "+ favoriteLanguage +" people.";
+console.log(codingFact);
+codingFact = favoriteLanguage;
+console.log(codingFact);
+codingFact = "The "+ favoriteLanguage +" have made little contribution to open-source code and its research and development.";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
+
