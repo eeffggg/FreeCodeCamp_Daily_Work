f8d9dafdb4c73536f8d33aec932e0d551e5631de
eeffggg <79692191+eeffggg@users.noreply.github.com>
2026-03-31 15:41:12 +0800

    Add two mad libs stories in story.js

---
+++ 3_31_JS_Robot_Self-Introduction/story.js
@@ -0,0 +1,19 @@
+let adjective = "lonely";
+let noun = "book";
+let verb = "speak";
+let place = "park";
+let adjective2 = "sad";
+let noun2 = "game";
+
+let firstStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("First story: "+firstStory);
+
+ adjective = "happy";
+ noun = "teacher";
+ verb = "teach";
+ place = "library";
+ adjective2 = "sunny";
+ noun2 = "student";
+
+let secondStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+"."
+console.log("Second story: "+secondStory);
