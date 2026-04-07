a60638485b47646fa8ea88157a4bf92ceb2ffd08
eeffggg <79692191+eeffggg@users.noreply.github.com>
2026-04-07 12:36:39 +0800

    Add functions for string manipulation and array handling

---
+++ 4_7_JS/for_of_lab.js
@@ -0,0 +1,61 @@
+//reverse a string
+function reverseString(arr) {
+  return arr.split("").reverse().join("");
+}
+//find the biggest number in each member array of the 2D-array;
+function largestOfAll(twoDArr) {
+  let resultArr = [];
+  for (let x of twoDArr) {
+    let maxNumber = -Infinity;
+    for (let y of x) {
+      if (maxNumber < y) {
+        maxNumber = y;
+      }
+    }
+    resultArr.push(maxNumber);
+  }
+  return resultArr;
+}
+//element finder
+function findElement(arr, func) {
+  for (let x of arr) {
+    if (func(x)) {
+      return x;
+    }
+  }
+  return undefined;
+}
+//slicing and splicing
+function frankenSplice(arr1, arr2, index) {
+  let arr3 = [];
+  for (let i = 0; i < index; i++) {
+    arr3.push(arr2[i]);
+  }
+  for (let x of arr1) {
+    arr3.push(x);
+  }
+  for (let j = index; j < arr2.length; j++) {
+    arr3.push(arr2[j]);
+  }
+  return arr3;
+}
+console.log(frankenSplice([1, 2, 3], [4, 5], 1));
+////如何表现字符串金字塔?
+// console.log("0\n000");
+//如何添加空格
+// console.log(" ".repeat(3) + "0");
+function pyramid(str, height, boolean) {
+  let string1 = "\n";
+  if (!boolean) {
+    for (let x = 1; x <= height; x++) {
+      string1 += " ".repeat(height - x) + str.repeat((x - 1) * 2 + 1) + "\n";
+    }
+    return string1;
+  }
+  let string2 = "\n";
+  for (let y = height; y >= 1; y--) {
+    string2 += " ".repeat(height - y) + str.repeat((y - 1) * 2 + 1) + "\n";
+  }
+  return string2;
+}
+console.log(pyramid("o", 4, false));
